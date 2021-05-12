2397ab9da8b4ae6692b49d16d08a9339b69d6cbc
Suraj <surajbhansingh588@gmail.com>
2021-05-12 08:17:09 +0530

    Added media queries

--- Portfolio/src/Components/Footer.jsx
+++ Portfolio/src/Components/Footer.jsx
@@ -2,26 +2,23 @@ import React from "react"
 import styles from "./Footer.module.css";
 import {FaGithubSquare,FaLinkedin} from "react-icons/fa";
 import {HiMail} from "react-icons/hi"
-import { useHistory } from "react-router";
 
 const Footer = () => {
-    const history = useHistory();
-
-    const contactPage = () => {
-        history.push("/contact");
-    }
+    
     return (
         <>
             <div className={styles.cover}>
                 <div>
                     <div>Let's Work Together</div>
                     <div>Have a project you would like to discuss?</div>
-                    <button onClick={contactPage}>Get in Touch</button>
+                    <a href="/contact/#contact">
+                        <button>Get in Touch</button>
+                    </a>
                 </div>
                 <div>
                     <div>
                     <HiMail style={{fontSize:"32px",color:"white"}}/>
-                        <div>surajbhansingh588@gmail.com</div>
+                        <div className={styles.email}>surajbhansingh588@gmail.com</div>
                     </div>
                     <div>
                     <FaLinkedin style={{color:"white",fontSize:"32px"}}/>
