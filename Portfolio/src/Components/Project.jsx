7fac1181ae05a69e4b2b6b1c79ae53be09f92c58
Suraj <surajbhansingh588@gmail.com>
2021-05-15 16:54:06 +0530

    External links now open in new tab

--- Portfolio/src/Components/Project.jsx
+++ Portfolio/src/Components/Project.jsx
@@ -15,17 +15,17 @@ const Project = ({project}) => {
                 <TechUsed techUsed={project.tech_stack}/>
                 <div>
                     <div>
-                        <a href={project.github_link}>
+                        <a href={project.github_link} target="_blank">
                         <ImGithub style={{color:"black",fontSize:"52px"}}/>
                         </a>
                     </div>
                     <div>
-                        <a href={project.medium_link}>
+                        <a href={project.medium_link} target="_blank">
                             <img src="Medium.png" alt="medium"/>
                         </a>
                     </div>
                     <div>
-                        <a href={project.live}>
+                        <a href={project.live} target="_blank">
                             <img src="Live.png" alt="Live"/>
                         </a>
                     </div>
